a872ff45d0965434749896b98dfbb481eb4b13f3
jimmyd-be <dom.jimmy@gmail.com>
2023-06-16 13:12:39 +0200

    Better handling of losing wifi connectino

--- DHTToHass/DHTToHass.ino
+++ DHTToHass/DHTToHass.ino
@@ -3,30 +3,23 @@
 #include <WiFiNINA.h>
 #include <ArduinoJson.h>
 #include <ArduinoMqttClient.h>
+#include <utility/wifi_drv.h>
 
 #include "arduino_secrets.h"
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;    // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 int status = WL_IDLE_STATUS;  // the Wifi radio's status
-
-char servername[]="google.com";  // remote server we will connect to
+String stateTopic = "home/temperature_sensor_living_room/state";
+const char broker[] = BROKER_IP;
+int port = 1883;
 
 WiFiClient client;
 MqttClient mqttClient(client);
 
-const char broker[] = "192.168.0.250";
-int        port     = 1883;
-const char topic[]  = "arduino/simple";
-
-const long interval = 1000;
-unsigned long previousMillis = 0;
-
 DHT dht;
 
 #define DHT22_PIN 2
-
-String stateTopic = "home/temperature_sensor_living_room/state";
 
 
 void sendMQTTTemperatureDiscoveryMsg() {
@@ -36,7 +29,7 @@ void sendMQTTTemperatureDiscoveryMsg() {
   StaticJsonDocument<256> doc;
 
   doc["name"] = "Living room Temperature";
-  doc["stat_t"]   = stateTopic;
+  doc["stat_t"] = stateTopic;
   doc["unit_of_meas"] = "°C";
   doc["dev_cla"] = "temperature";
   doc["frc_upd"] = true;
@@ -56,7 +49,7 @@ void sendMQTTHumidityDiscoveryMsg() {
   StaticJsonDocument<256> doc;
 
   doc["name"] = "Living room Humidity";
-  doc["stat_t"]   = stateTopic;
+  doc["stat_t"] = stateTopic;
   doc["unit_of_meas"] = "%";
   doc["dev_cla"] = "humidity";
   doc["frc_upd"] = true;
@@ -69,43 +62,81 @@ void sendMQTTHumidityDiscoveryMsg() {
   mqttClient.endMessage();
 }
 
-void connectWifi() {
+void changeLedColor(String color) {
 
-  if (status != WL_CONNECTED) {
-    // attempt to connect to Wifi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network:
-    status = WiFi.begin(ssid, pass);
-
-    // wait 10 seconds for connection:
-    delay(10000);
-  }
-
-  // you're connected now, so print out the data:
-  Serial.print("You're connected to the network");
-
-  Serial.print("Attempting to connect to the MQTT broker: ");
-  Serial.println(broker);
-
-  if (!mqttClient.connect(broker, port)) {
-    Serial.print("MQTT connection failed! Error code = ");
-    Serial.println(mqttClient.connectError());
-
-    while (1);
-  }
-
-  Serial.println("You're connected to the MQTT broker!");
+  if (color == "red") {
+    WiFiDrv::analogWrite(25, 0);    //GREEN
+    WiFiDrv::analogWrite(26, 100);  //RED
+    WiFiDrv::analogWrite(27, 0);    //BLUE
+  } else if (color == "green") {
+    WiFiDrv::analogWrite(25, 0);  //GREEN
+    WiFiDrv::analogWrite(26, 0);  //RED
+    WiFiDrv::analogWrite(27, 0);  //BLUE
   }
 }
 
-void setup()
+char ScanSSIDs()
+//scan SSIDs, and if my SSID is present return 1
 {
+  char score = 0;
+  int numSsid = WiFi.scanNetworks();
+  if (numSsid == -1) return (0);  //error
+  for (int thisNet = 0; thisNet < numSsid; thisNet++)
+    if (strcmp(WiFi.SSID(thisNet), ssid) == 0) score = 1;  //if one is = to my SSID
+  return (score);
+}
+
+void testWiFiConnection()
+//test if always connected
+{
+  int StatusWiFi = WiFi.status();
+  if (StatusWiFi == WL_CONNECTION_LOST || StatusWiFi == WL_DISCONNECTED || StatusWiFi == WL_SCAN_COMPLETED)  //if no connection
+  {
+    changeLedColor("red");
+    if (ScanSSIDs()) connectWifi();  //if my SSID is present, connect
+  }
+}
+
+void connectWifi()
+//connect to my SSID
+{
+  status = WL_IDLE_STATUS;
+  while (status != WL_CONNECTED) {
+    status = WiFi.begin(ssid, pass);
+    if (status == WL_CONNECTED) digitalWrite(9, HIGH);  //LED ON to show connected
+    else delay(500);
+  }
+
+  changeLedColor("green");
+  // you're connected now, so print out the data:
+  Serial.println("You're connected to the network");
+
+  Serial.println("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.println("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1)
+      ;
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+}
+
+void setup() {
   Serial.begin(9600);
   Serial.println("Attempting to connect to WPA network...");
   Serial.print("SSID: ");
   Serial.println(ssid);
+
+  //Led configuration
+  WiFiDrv::pinMode(25, OUTPUT);
+  WiFiDrv::pinMode(26, OUTPUT);
+  WiFiDrv::pinMode(27, OUTPUT);
+
+  changeLedColor("red");
 
   // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
@@ -127,14 +158,12 @@ void setup()
 
   // initialize the DHT sensor
   dht.setup(DHT22_PIN);
-
 }
 
-void loop()
-{
+void loop() {
   delay(dht.getMinimumSamplingPeriod());
 
-  connectWifi();
+  testWiFiConnection();
 
   float temperature = dht.getTemperature();
   float humidity = dht.getHumidity();
@@ -146,7 +175,7 @@ void loop()
   String json;
 
   doc["humidity"] = humidity;
-  doc["temperature"]   = temperature;
+  doc["temperature"] = temperature;
 
   serializeJson(doc, json);
 
